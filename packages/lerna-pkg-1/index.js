e5cbac43fa6d581bf181fe67d9aca6f1300f6b16
Truong Nguyen <tnguyen@demystdata.com>
2021-06-26 08:31:40 +1000

    feat: added support sumArr function

--- packages/lerna-pkg-1/index.js
+++ packages/lerna-pkg-1/index.js
@@ -3,3 +3,7 @@ import { format } from "@samuraitruong/lerna-pkg-2";
 export function sum(a, b) {
   return format(a+b)
 }
+
+export function sumArr(...args) {
+  return args.reduce((a,b) => a+b,0)
+}
